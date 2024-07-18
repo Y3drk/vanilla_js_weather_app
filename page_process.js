7dc66e4e5e2a584f530ac1900536d8080eb3906e
Y3drk <ziebura.jedrzej@gmail.com>
2024-07-18 13:09:16 +0200

    Main js functionalities - API calls

--- page_process.js
+++ page_process.js
@@ -6,26 +6,88 @@ resetButton.addEventListener("click", resetResults, false);
 
 const baseOutput = "Type the name of the city in the input field, then click SEARCH to get its weather";
 const waitingOutput = "Your call is being processed, please wait";
+const successfulCall = "Your call was successful! Here are the results for ";
+
+const output = document.getElementById("output_control_text");
+output.textContent = baseOutput;
+
+const WEATHER_API_KEY = "3127cee09dcf94afcea237915dc5cb77";
+// For best practice should be hidden, but requires additional tools/libs
+
+function getGeocodingAPILink(location){
+    return `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${WEATHER_API_KEY}`;
+}
+
+function getCurrentWeatherAPILink(latitude, longitude){
+    return `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}&units=metric`
+}
 
 function returnErrorMessage(error){
-    return `The API call failed due to ${error} error!`;
+    return `The API call failed due to ${error}`;
 }
 
-function callAPI(e) {
+async function callAPI(e) {
     e.preventDefault()
 
+    const input_field = document.getElementById('place_input');
+
     //1. Set-up waiting for results in display
+    output.textContent = waitingOutput;
 
     //2. Call weather API
+    const location = input_field.value;
 
-    //3. Display results or error
+    //a. Convert location name to latitude and longitude
+    try {
+        const response = await fetch(getGeocodingAPILink(location));
+        if (!response.ok) {
+            returnErrorMessage(`(GeoCoding) - response status: ${response.status}`);
+            return;
+        }
+
+        const geocodingJson = await response.json();
+        const latitude = geocodingJson[0].lat;
+        const longitude = geocodingJson[0].lon;
+
+        //b. call the API for current weather
+        const weatherResponse = await fetch(getCurrentWeatherAPILink(latitude, longitude));
+        if (!response.ok) {
+            returnErrorMessage(`(Current Weather) - response status: ${response.status}`);
+            return;
+        }
+
+        const weatherJson = await weatherResponse.json();
+        const temperature = Math.round(weatherJson.main.temp); //deg C
+        const clouds = weatherJson.clouds.all; // %
+        const wind = weatherJson.wind.speed; // m/s
+        const weather = weatherJson.weather[0].main; //string
+        const weatherDescription = weatherJson.weather[0].description; //string
+        const pressure = weatherJson.main.pressure; // hPa
+
+        console.log(weather, weatherDescription, wind, clouds, temperature, pressure);
+
+
+        //3. Display results or error
+        output.textContent = `${successfulCall} ${location}.`
+
+        // const weatherInfo = document.createElement('ul');
+        //
+        // document.getElementById('output_div').appendChild(weatherInfo);
+
+    } catch (error) {
+        returnErrorMessage(error.message);
+    }
 
     //4. Clear input field
+    input_field.value = "";
 }
-
 
 function resetResults(e) {
     e.preventDefault()
 
-    //4. Go back to the base output
+    //1. return to default control text
+    output.textContent = baseOutput;
+
+    //2. clear city weather output
+
 }
